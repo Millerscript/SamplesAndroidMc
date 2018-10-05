9f6ca6673a2805488a3c58e1d9179ef3d5a853c3
miller.mosquera <miller.mosquera@etn.com.co>
2018-10-05 15:59:01 -0500

    Change in class FragmentDatail

---
+++ app/src/main/java/com/arq/millermosquera/samplesmc/TextWatcherCurrencyFormat.java
@@ -0,0 +1,49 @@
+package com.arq.millermosquera.samplesmc;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class TextWatcherCurrencyFormat implements TextWatcher {
+
+    private EditText editText;
+    private TextInputLayout tilEditText;
+
+    private String current = "";
+
+    public  TextWatcherCurrencyFormat(EditText editText){
+        this.editText = editText;
+    }
+
+    public  TextWatcherCurrencyFormat(EditText editText,TextInputLayout tilEditText){
+        this.editText = editText;
+        this.tilEditText  = tilEditText;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        if(tilEditText!=null){
+            tilEditText.setError(null);
+            tilEditText.setErrorEnabled(false);
+        }
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        editText.setError(null);
+        if(!charSequence.toString().equals(current) && !charSequence.toString().isEmpty()){
+            if ("$".equals(charSequence.toString())) {
+                current = "";
+            } else {
+                current = TextUtils.getMoneyFormat(charSequence.toString().replaceAll("[^0-9]", ""));
+            }
+            editText.setText(current);
+            editText.setSelection(current.length());
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
